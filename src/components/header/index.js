2520d23903f5bd45c3a89fc4073c2a6733ca7aa4
MartaUlasen <marta.ulasen@gmail.com>
2018-08-27 17:52:43 +0300

    Added styles

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -4,30 +4,21 @@ import './header.css';
 import { Link } from 'react-router-dom';
 
 class Header extends Component {
-  render() {
-    return (
-        <header>
-            <div className="container">
-                <div className="logo mx-auto">
-                    <Link to="/">
-                        LOGO
-                    </Link>
-                </div>
-                    <Navigation />
-                    <ul className="nav nav-pills nav-filter invisible">
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" data-toggle="dropdown" href="#" 
-                            role="button" aria-haspopup="true" aria-expanded="false">Sort by</a>
-                            <div className="dropdown-menu">
-                                <button className="dropdown-item js-sorter--date" href="#">Date of reliase</button>
-                                <button className="dropdown-item js-sorter--episode" href="#">Number of episode</button>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-        </header>
-    );
-  }
+	render() {
+		return (
+			<header>
+				<div className="container">
+					<div className="logo mx-auto">
+						<Link to="/">
+							LOGO
+						</Link>
+					</div>
+						<Navigation />
+						
+					</div>
+			</header>
+		);
+	}
 }
 
 export default Header;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './customTheme.css';
+import './bootstrap.min.css';
+import './styles.scss';
+
 import App from './components/app';
 import { BrowserRouter } from 'react-router-dom';
 
 
 ReactDOM.render((
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+	<BrowserRouter>
+    	<App />
+	</BrowserRouter>
 ), document.getElementById('root'))
