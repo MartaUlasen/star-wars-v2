7af847682ed6c2e913f07c324feffef58ae7e6b4
MartaUlasen <marta.ulasen@gmail.com>
2019-03-13 13:03:18 +0300

    Remoted excess actions on actions/films.js

--- src/actions/films.js
+++ src/actions/films.js
@@ -7,9 +7,6 @@ export const REQUEST_FILMS = 'REQUEST_FILMS';
 export const REQUEST_FILMS_SUCCESS = 'REQUEST_FILMS_SUCCESS';
 export const REQUEST_FILMS_ERROR = 'REQUEST_FILMS_ERROR';
 
-export const REQUEST_CHARACTERS = 'REQUEST_CHARACTERS';
-export const RECEIVE_CHARACTERS = 'RECEIVE_CHARACTERS';
-
 function requestFilms() {
     return {
         type: REQUEST_FILMS,
