b62f8ca9765365bbc4016c6118f377639e69799c
MartaUlasen <marta.ulasen@gmail.com>
2019-04-27 23:05:37 +0300

    Updates

---
+++ precache-manifest.946376b9bd83d52c2f00c88addcc50e1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29823ccbfa61bbf49bb6f4595465745a",
+    "url": "/star-wars-v2/index.html"
+  },
+  {
+    "revision": "96936d8389cf821d8193",
+    "url": "/star-wars-v2/static/css/2.acb72abc.chunk.css"
+  },
+  {
+    "revision": "9c732e0a43a73b67d7c7",
+    "url": "/star-wars-v2/static/css/main.16c5be9d.chunk.css"
+  },
+  {
+    "revision": "96936d8389cf821d8193",
+    "url": "/star-wars-v2/static/js/2.3bd19b0f.chunk.js"
+  },
+  {
+    "revision": "9c732e0a43a73b67d7c7",
+    "url": "/star-wars-v2/static/js/main.b625c0bf.chunk.js"
+  },
+  {
+    "revision": "a098f28153868570822a",
+    "url": "/star-wars-v2/static/js/runtime~main.d18b7344.js"
+  },
+  {
+    "revision": "53a7ce44b0737c387d78f35654ae56b2",
+    "url": "/star-wars-v2/static/media/bb8.53a7ce44.svg"
+  },
+  {
+    "revision": "8f11ca7ae0a8d092c660c98191a6c4fe",
+    "url": "/star-wars-v2/static/media/r2d2.8f11ca7a.svg"
+  },
+  {
+    "revision": "0622e026419b3ec63617037e4e42df7e",
+    "url": "/star-wars-v2/static/media/space_640.0622e026.png"
+  }
+]);
